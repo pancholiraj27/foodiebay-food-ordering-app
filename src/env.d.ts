3538449a20f3b5da578ed4c589198176e13e3cf7
Raj Pancholi <pancholiraj27@gmail.com>
2023-08-07 17:14:32 +0530

    changes in .env file

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,8 +1,8 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      LOGIN_DOMAIN: "dev-k47bhp0g634l05wp.us.auth0.com";
-      LOGIN_CLIENT_ID: "4XlFN3IySxabQossVGLB7VEaFtDe8sxA";
+      REACT_APP_LOGIN_DOMAIN: string;
+      REACT_APP_LOGIN_CLIENT_ID: string;
     }
   }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -7,10 +7,13 @@ import { Auth0Provider } from "@auth0/auth0-react";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
+console.log(process.env);
+
 root.render(
   <Auth0Provider
-    domain={process.env.LOGIN_DOMAIN}
-    clientId={process.env.LOGIN_CLIENT_ID}
+    domain={process.env.REACT_APP_LOGIN_DOMAIN}
+    clientId={process.env.REACT_APP_LOGIN_CLIENT_ID}
     authorizationParams={{
       redirect_uri: window.location.origin,
     }}
@@ -18,3 +21,6 @@ root.render(
     <App />
   </Auth0Provider>
 );
+
+// domain="dev-k47bhp0g634l05wp.us.auth0.com"
+// clientId="4XlFN3IySxabQossVGLB7VEaFtDe8sxA"
